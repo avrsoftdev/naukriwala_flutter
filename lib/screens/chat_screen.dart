23b39b9c72b06ce5bd7ad1f7b12054d0488f6550
Aakash Dodwal <naukariwalaav@gmail.com>
2025-10-19 00:13:23 +0530

    api integration

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: deprecated_member_use
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -482,7 +481,13 @@ class _ChatScreenState extends State<ChatScreen> {
       if (recipientCollection == 'RecruiterNotifications') {
         await _validateAndCreateApplication(widget.jobId, widget.recipientId);
       }
-      await _authService.sendMessage(widget.recipientId, widget.jobId, message, status: 'sent');
+      await _authService.sendMessage(
+        senderId: senderId,
+        recipientId: widget.recipientId,
+        jobId: widget.jobId,
+        message: message,
+        status: 'sent',
+      );
       _messageController.clear();
       if (mounted) {
         setState(() {
@@ -542,7 +547,13 @@ class _ChatScreenState extends State<ChatScreen> {
       if (recipientCollection == 'RecruiterNotifications') {
         await _validateAndCreateApplication(widget.jobId, widget.recipientId);
       }
-      await _authService.sendMessage(widget.recipientId, widget.jobId, initialMessage, status: 'sent');
+      await _authService.sendMessage(
+        senderId: senderId,
+        recipientId: widget.recipientId,
+        jobId: widget.jobId,
+        message: initialMessage,
+        status: 'sent',
+      );
       _messageController.clear();
       if (mounted) {
         setState(() {
