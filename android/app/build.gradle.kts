0ec7ba7aa0b31ee05b6c0841206421ce948c0400
Aakash Dodwal <naukariwalaav@gmail.com>
2025-10-24 19:58:31 +0530

    Revert

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("kotlin-android")
     id("com.google.gms.google-services")
 }
 
@@ -28,12 +29,12 @@ android {
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     signingConfigs {
