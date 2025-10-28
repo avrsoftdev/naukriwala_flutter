3f7b671d02200ed4eca1be5d2bf04e3d5033b265
Aakash Dodwal <naukariwalaav@gmail.com>
2025-10-28 15:57:26 +0530

    Changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -23,7 +23,7 @@ void main() {
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.dsdssdsds
+    // Verify that our counter has incremented.dsdssdsds fsdrfe
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
