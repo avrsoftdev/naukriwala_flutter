4e13b717da58c1fc2ffba0c83b9eb5b95d037df3
Aakash Dodwal <naukariwalaav@gmail.com>
2026-02-13 11:32:20 +0530

    testing push

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -305,7 +305,7 @@ class _ChatScreenState extends State<ChatScreen> with WidgetsBindingObserver {
   Future<Map<String, String>> _fetchRecipientInfo() async {
     final uid = _auth.currentUser?.uid;
     if (uid == null) return {'recipientName': 'Unknown', 'company': 'Unknown'};
-//commentssss
+//commentssss3232
     String name = 'Unknown';
     String company = 'Unknown';
     for (int i = 0; i < 3; i++) {
